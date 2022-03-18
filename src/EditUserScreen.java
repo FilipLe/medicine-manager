f4fbe91356053d0d1039746df685d5c66fddca44
nguyenle <nguyenle@localhost>
2022-03-18 09:34:45 +0100

    Program Complete

--- src/EditUserScreen.java
+++ src/EditUserScreen.java
@@ -62,6 +62,7 @@ public class EditUserScreen {
 		 * Title
 		 */
 		frame = new JFrame("Equipment Management System");
+		frame.getContentPane().setBackground(new Color(135, 206, 235));
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
